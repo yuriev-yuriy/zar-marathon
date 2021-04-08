06519edb491c7378a3d6fe2f27cf057f98ec81f7
Yuriev Yuriy <yuriev1986@gmail.com>
2021-04-08 22:08:00 +0300

    something done

--- main.js
+++ main.js
@@ -18,17 +18,21 @@ const player2 = {
     }
 }
 
-const createPlayer = function (class, name, hp) {
+const createPlayer = function (playerClass, name, hp) {
     const $div = document.createElement('div');
-    $div.classList.add(player1);
+    $div.classList.add(playerClass);
     const $divProgress = document.createElement('div');
+    $divProgress.classList.add('progressbar');
     const $divCharacter = document.createElement('div');
+    $divCharacter.classList.add('character');
     const $divLife = document.createElement('div');
+     $divCharacter.classList.add('life');
     const $divName = document.createElement('div');
+     $divName.classList.add('name');
 
     const $img = document.createElement('img');
     $img.src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif";
-    $divLife.style.width = '100%';
+    $divLife.style.width = hp + '%';
     $divName.innerText = name;
     $divProgress.appendChild($divLife);
     $divProgress.appendChild($divCharacter);
@@ -38,9 +42,10 @@ const createPlayer = function (class, name, hp) {
     const $divArenas = document.querySelector('.arenas');
     $divArenas.appendChild($div);
 }
+console.log(document);
 
 createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'SUB-ZERO', 80);
+createPlayer('player2', 'Malina', 80);
 
-createPlayer('player1', player1);
-createPlayer('player2', player1);
+// createPlayer('player1', player1);
+// createPlayer('player2', player2);
